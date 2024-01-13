b6c059d7e42f7b8c33c99ae6b3823fae3dc3b9f8
yeonhub-desktop <lsy3237@gmail.com>
2024-01-14 03:38:40 +0900

    [TEST] 렌더링 성능 테스트 코드 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import styled from "styled-components";
 import Todays from '../components/Todays';
 
+const onRenderCallback = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+) => {
+    console.log({ id, phase, actualDuration, baseDuration, startTime, commitTime, interactions });
+};
+
 const Home = () => {
     return (
-        <>
+        <Profiler id="Home" onRender={onRenderCallback}>
             <Todays />
-        </>
+        </Profiler>
     );
 };
 
