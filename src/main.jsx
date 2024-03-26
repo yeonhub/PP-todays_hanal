889f3fa411f4543c04b537d2445de3344350d401
yeonhub <lsy3237@gmail.com>
2024-03-26 13:43:34 +0900

    [FIX] React 18 대응createRoot root API 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './store/index.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './store/index.jsx';
 import GlobalStyle from '../Global';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <GlobalStyle/>
+    <GlobalStyle />
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
